884f10cb2f7e46527732fbe55a0419803f517f3a
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2025-03-06 10:23:26 -0300

    fiz o get por id e o metodo para criar um usuario e configurei para receber json no index

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -6,3 +6,29 @@ export const getAll = async (req: Request, res: Response) => {
   const users = await UserModel.findAll();
   res.send(users);
 };
+
+// método que busca por id
+export const getUserById = async (
+  req: Request<{ id: string }>,
+  res: Response
+) => {
+  const user = await UserModel.findByPk(req.params.id);
+
+  return res.json(user);
+};
+
+// método que cria usuário
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+
+    if (!name || name === "") {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const user = await UserModel.create({ name });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json("Erro interno no servidor " + error);
+  }
+};
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAll } from "../controller/UserController";
+import { getAll, getUserById, createUser } from "../controller/UserController";
 
 const router = express.Router();
 
 router.get("/users", getAll);
+router.get("/users/:id", getUserById);
+router.post("/users", createUser);
 
 export default router;
