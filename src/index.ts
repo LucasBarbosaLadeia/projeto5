81139b25f50b9e2234e81ab129fb6dec378650e9
lucas <lucasladeia16@gmail.com>
2025-03-10 19:56:43 -0300

    corrigindo erro de commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 import express from "express";
 import sequelize from "./config/database";
-import userRoutes from "./routes/UserRoutes";
+import userRoute from "./routes/UserRoutes";
 import actorRoutes from "./routes/ActorRoutes";
 const app = express();
 const port = 3000;
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
-app.use(userRoutes);
+app.use(userRoute);
 app.use(actorRoutes)
 
 sequelize
@@ -26,31 +26,3 @@ sequelize
 app.listen(port, () => {
   console.log("Server is running on port ", port);
 });
-=======
-import express from "express";
-import sequelize from "./config/database";
-import userRoutes from "./routes/UserRoutes";
-
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.use(express.json());
-app.use(userRoutes);
-
-sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("database foi sincronizado com sucesso");
-  })
-  .catch((error) => {
-    console.log("deu zica no bagulho", error);
-  });
-
-app.listen(port, () => {
-  console.log("Server is running on port ", port);
-});
->>>>>>> 31eb1faf6aeb4217d2c6a415ba5b664c7c5134bc
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
 import express from "express";
 import {
   getAll,
@@ -19,27 +16,3 @@ router.put("/users/:id", updateUser);
 router.delete("/users/:id", updateUser);
 
 export default router;
-=======
->>>>>>> 778591ede6f61dbc95df2b6ef16853d151e64af2
-import express from "express";
-import {
-  getAll,
-  getUserById,
-  createUser,
-  updateUser,
-} from "../controller/UserController";
-
-const router = express.Router();
-
-router.get("/users", getAll);
-router.get("/users/:id", getUserById);
-router.post("/users", createUser);
-router.put("/users/:id", updateUser);
-router.delete("/users/:id", updateUser);
-
-<<<<<<< HEAD
-export default router;
-=======
-export default router;
->>>>>>> 31eb1faf6aeb4217d2c6a415ba5b664c7c5134bc
->>>>>>> 778591ede6f61dbc95df2b6ef16853d151e64af2
