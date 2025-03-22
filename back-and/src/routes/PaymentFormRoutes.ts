1baa9e1db76a0fcf9e1c39fcb4c34719a82b5242
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2025-03-22 17:26:57 -0300

    teste de criar a pasta back and

---
+++ back-and/src/routes/PaymentFormRoutes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import {
+  getAll,
+}  from "../controller/PaymentFormController";
+  const router = express.Router();
+
+  router.get("/paymentForm", getAll);
+
+  export default router;
