6fdf21a115bde1b8d8c16ac0df63e25b57436410
lucas <lucasladeia16@gmail.com>
2025-06-09 21:47:10 -0300

    chore(commit): automatizacao do cadastro

--- tests/cadastro.spec.ts
+++ tests/cadastro.spec.ts
@@ -11,9 +11,11 @@ test("Cadastro de usuario", async ({ page }) => {
     await page.getByRole("link", { name: "cadastre-se" }).click()
    
     await expect(page).toHaveURL("http://localhost:/signup")
+    const random = Math.floor(Math.random() * 100000);
+const email = `user${random}@test.com`;
 
     await page.getByPlaceholder("nome").fill("nome")
-    await page.getByPlaceholder("E-mail").fill("emailti@gmail.com")
+    await page.getByPlaceholder("E-mail").fill(email)
     await page.getByPlaceholder("CPF").fill("11770764976")
     await page.getByPlaceholder("Senha").fill("12345678")
     await page.getByPlaceholder("Confirme").fill("12345678")
@@ -25,7 +27,7 @@ test("Cadastro de usuario", async ({ page }) => {
 console.log("URL atual:", page.url());
    expect(title).toBeTruthy()
 
-    await page.getByPlaceholder("Email").fill("emailti@gmail.com")
+    await page.getByPlaceholder("Email").fill(email)
 await page.getByPlaceholder("Senha").fill("12345678")
 
     await page.getByRole("button", { name: "Entrar" }).click()
