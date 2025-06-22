f80a439a229f537c064e1d1f6c65f700a59f263a
Gabrielhss06 <161782014+Gabrielhss06@users.noreply.github.com>
2025-06-22 15:46:28 -0300

    style(docker-compose): formatar YAML para consistência e legibilidade

--- tests/cadastro.spec.ts
+++ tests/cadastro.spec.ts
@@ -11,17 +11,32 @@ test("Cadastro de usuario", async ({ page }) => {
     await page.getByRole("link", { name: "cadastre-se" }).click()
    
     await expect(page).toHaveURL("http://localhost:/signup")
+    const random = Math.floor(Math.random() * 100000);
+const email = `user${random}@test.com`;
 
     await page.getByPlaceholder("nome").fill("nome")
-    await page.getByPlaceholder("E-mail").fill("kfsssssyjddk@gmail.com")
-    await page.getByPlaceholder("CPF").fill("11770764976")
+    await page.getByPlaceholder("E-mail").fill(email)
+    await page.getByPlaceholder("CPF").fill("94999548054")
     await page.getByPlaceholder("Senha").fill("12345678")
     await page.getByPlaceholder("Confirme").fill("12345678")
 
     await page.getByRole("button", { name: "criar conta" }).click()
-
-    await expect(page).toHaveURL("http://localhost:/signup");
-    await page.waitForTimeout(1000); // só para testar se redireciona com atraso
+     await page.waitForTimeout(3000); 
+    await expect(page).toHaveURL("http://localhost:/");
+    await page.waitForTimeout(1000);
 console.log("URL atual:", page.url());
+   expect(title).toBeTruthy()
+
+    await page.getByPlaceholder("Email").fill(email)
+await page.getByPlaceholder("Senha").fill("12345678")
+
+    await page.getByRole("button", { name: "Entrar" }).click()
    
+    await expect(page).toHaveURL("http://localhost:/home")
+
+    await expect(page.getByRole("link", { name: "Perfil" })).toBeVisible();
+    await page.getByRole("link", { name: "Perfil" }).click()
+    await page.getByRole("button", { name: "Deletar Perfil" }).waitFor();
+    await page.getByRole("button", { name: "Deletar Perfil" }).click()
+      await page.waitForTimeout(3000); 
 })
--- tests/crudmovies.spec.ts
+++ tests/crudmovies.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Listing movies", async ({ page }) => {
+test("crud de movies", async ({ page }) => {
     await page.goto("http://localhost/")
     const title = await page.getByText("Login")
 
@@ -14,26 +14,67 @@ test("Listing movies", async ({ page }) => {
     await expect(page).toHaveURL("http://localhost/home")
     await expect(page.getByRole("link", { name: "createMovies" })).toBeVisible();
     await page.getByRole("link", { name: "createMovies" }).click()
+//listagem
     await expect(page.getByText('ID')).toBeVisible
+//falha de listagem
+    const falha = page.getByText('homem aranha');
+    await expect(falha).not.toBeVisible();
 
    
-await page.locator("button", { hasText: "editar - $10" }).click();
-    await page.getByLabel("Nome").fill("Novo Filme");
-    await page.getByLabel("Descrição").fill("Descriç atualizada");
-    await page.getByLabel("Data de Lançamento").fill("2025-04-04T00:00:00.000Z");
-    await page.getByRole("button",{name: "salvar alterações"}).click()
 
+//outra listagem 
     await expect(page.getByText('Novo Nome do Filme')).toBeVisible
-
+//criação do filme
+    const random = Math.floor(Math.random() * 100000);
+    const nome = `${random}`;
     await page.getByRole("button",{name: "Novo filme"}).click()
-    await page.getByLabel("Nome").fill("dinosaro");
+    await page.getByLabel("Nome").fill(nome);
     await page.getByLabel("Descrição").fill("Descrição");
     await page.getByLabel("Imagens").fill("https://br.web.img3.acsta.net/pictures/15/10/15/22/24/429658.jpg");
     await page.getByLabel("Data de Lançamento").fill("2024-04-04T00:00:00.000Z");
     await page.getByLabel("IDs dos Atores (separados por vírgula)").fill("1");
     await page.getByRole("button",{name: "salvar"}).click()
 
+    const testefilme = page.getByText('dinosaro');
+    await expect(await testefilme.count()).toBeGreaterThan(0);
+
+//falha de criação
+    await page.getByRole("button",{name: "Novo filme"}).click()
+    await page.waitForSelector('input[id="field-name"]');
+    await page.getByLabel("Nome").fill("cachorro");
+    await page.getByLabel("Descrição").fill("Descrição");
+    await page.getByLabel("Imagens").fill("https://br.web.img3.acsta.net/pictures/15/10/15/22/24/429658.jpg");
+    await page.getByLabel("Data de Lançamento").fill("wifggvcldgc");
+    await page.getByLabel("IDs dos Atores (separados por vírgula)").fill("1");
+    await page.getByRole("button",{name: "salvar"}).click()
     
+    await page.getByRole("button",{name: "Cancelar"}).click()
 
+    const testefilmefalha = page.getByText('cachorro');
+    await expect(await testefilmefalha.count()).toBeLessThan(1)
+//delete
+ await page.getByTestId(`delete-film-${nome}`).click();
+  await expect(page.locator('div', { hasText: nome })).toHaveCount(0);
+//delete falha
+    const card = page.locator('div', { hasText: 'Homem Aranha' });
 
+    const deleteButtonfalha = card.getByRole('button', { name: 'deletar' });
+    await expect(deleteButtonfalha).not.toBeVisible();
+//edição
+    await page.locator("button", { hasText: "editar" }).first().click();
+    await page.getByLabel("Nome").fill("Novo Filme");
+    await page.getByLabel("Descrição").fill("Descriç atualizada");
+    await page.getByLabel("Data de Lançamento").fill("2025-04-04T00:00:00.000Z");
+    await page.getByRole("button",{name: "salvar alterações"}).click()
+
+    await page.locator("button", { hasText: "editar" }).first().click();
+    await page.getByLabel("Nome").fill("Novo Filme");
+    await page.getByLabel("Descrição").fill("Descriç atualizada");
+    await page.getByLabel("Data de Lançamento").fill("fjnvsobgdb");
+    await page.getByRole("button",{name: "salvar alterações"}).click()
+
+    await expect(page.getByRole('button', { name:'Salvar Alterações'})).toBeVisible();
+    await page.getByRole("button",{name: "Cancelar"}).click()
+
+    
 })
